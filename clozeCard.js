b2281b55621294e05afbf27092d41e885a106625
Martins Ewere <martinsewere@gmail.com>
2017-07-17 18:33:30 -0700

    more updates

--- clozeCard.js
+++ clozeCard.js
@@ -8,6 +8,18 @@ exports.BasicCard  = function(front, back) {
 //clozeCard constructor takes two argument: text and cloze
 
 exports.ClozeCard = function(text, cloze) {
+
+	// Convert the incoming strings to lower case
+	var textToLower = text.toLowerCase();
+	var clozeToLower = cloze.toLowerCase();
+
+	// Confirm that the cloze statement appears within the complete text
+	if (!textToLower.includes(clozeToLower)) {
+		console.log('ERROR: cloze-deletion does not appear within full text -- <' + cloze + '>');
+		return;
+	}
+
+
 	this.text = text;
 	this.cloze = cloze;
 	this.partial = text.replace(cloze, '...');
